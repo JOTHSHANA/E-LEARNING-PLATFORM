152477ecbde239769f6399419f4c7d5cd23677b0
Priyadarshan-B <bpriyan18082004@gmail.com>
2024-09-18 12:42:46 +0530

    aos changes

--- frontend/src/pages/About/about.jsx
+++ frontend/src/pages/About/about.jsx
@@ -4,27 +4,27 @@ import "./about.css";
 const About = React.forwardRef((props, ref) => (
   <section ref={ref} id="about" className="about-section">
     <div className="about-content">
-      <div className="ribbon" data-aos="zoom-out">About Us</div>
+      <div className="ribbon" >About Us</div>
       {/* <video autoPlay muted loop className="about-video" data-aos="fade-right" data-aos-delay="100">
         
         Your browser does not support the video tag.
       </video> */}
       <div className="text-container">
         
-        <p className="about-p"data-aos="fade-left" data-aos-delay="300">
+        <p className="about-p">
           Learn more about our mission and values. Lorem ipsum dolor sit amet,
           consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed
           cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum
           imperdiet. Duis sagittis ipsum.
         </p>
-        <p className="about-p" data-aos="fade-left" data-aos-delay="500">
+        <p className="about-p" >
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
           odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
           quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent
           mauris. Fusce nec tellus sed augue semper porta. Mauris massa.
           Vestibulum lacinia arcu eget nulla.
         </p>
-        <p className="about-p" data-aos="fade-left" data-aos-delay="700">
+        <p className="about-p" >
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
           odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
           quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent
