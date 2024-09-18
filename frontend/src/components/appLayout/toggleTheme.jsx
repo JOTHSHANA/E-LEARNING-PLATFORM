c305195aed7d213e26fcb5f907d2a34e3c5cacc1
JOTHSHANA <jothshana123@gmail.com>
2024-09-18 12:39:53 +0530

    courses in landing page

--- frontend/src/components/appLayout/toggleTheme.jsx
+++ frontend/src/components/appLayout/toggleTheme.jsx
@@ -27,7 +27,7 @@ const lightModeProperties = {
     "--shadow": "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
     "--icons-rev": "#12c5d1",
     "--icons-bg": "#e5e3f3",
-    "--card": "#1d1f22",
+    "--card": "#c4c5cb",
     "--footer": "#eef0fe",
 
 };
